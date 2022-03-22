9fd47485aec885458b2ca221c19128c0332d6dfc
SantianLin <yanyulin@LAPTOP-M7MLC6NU.localdomain>
2022-03-22 03:35:47 -0400

    half way finishing all migration, basic code and logic in place, not with good style or CI-passing

--- inventory/management/commands/trans.py
+++ inventory/management/commands/trans.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from inventory.models import User, Family, Category, Item, ItemTransaction, Checkin, Checkout, AgeRange
+import re
 
 # DROP SCRIPT
 
@@ -7,27 +8,68 @@ class Command(BaseCommand):
     args = '<this func takes no args>'
     help = 'Run this script to change old database into newly designed one.'
 
-    def _update_users(self):
-        User.objects.all().delete() 
+    NEW_ITEM_NAMES = ["shirt", "shoes", "underwear", "socks", "pj", "shorts", "pants", "sweater/sweartshirt", "coat", "dress/skirt", "accessories", "other", "snowsuit", "onesie"]
 
-        print("Users deleted.")
+    def map(itemName):
+        if itemName == "snowsuit":
+            return "coat"
 
-    def _update_families(self):
-        Family.objects.all().delete() 
-
-        print("Families deleted.")
-
-    def _update_age_ranges(self):
-        AgeRange.objects.all().delete() 
-
-        print("AgeRanges deleted.")
-
-    def _update_categories(self):
-        Category.objects.all().delete() 
-
-        print("Categories deleted.")
 
     def _update_items(self):
+        for t in Item.objects.all():
+            name = t.name
+            x = re.match("^(boys|girls)", name)
+            if x:
+                print(name)
+                if name.startswith("boys "):
+                    name_new = name[len("boys "):]
+                elif name.startswith("girls "):
+                    name_new = name[len("girls "):]
+                print(name_new)
+                item_mapped = name_new.split()[0]
+                if (len(name_new.split()) > 1) and name_new.split()[1] == "socks":
+                    item_mapped = "socks"
+                if ("snowsuit" in item_mapped):
+                    item_mapped = "snowsuit"
+                print(item_mapped)
+
+                if ("teen" in name_new) or ("14-16" in name_new) or ("18-20" in name_new):
+                    size_mapped = "teen"
+                elif ("kid" in name_new) or ("6-7" in name_new) or ("8-10" in name_new) or ("10-12" in name_new) or ("12-14" in name_new):
+                    size_mapped = "kid"
+                elif ("infant" in name_new) or (" mo" in name_new):
+                    size_mapped = "baby"
+                elif ("2T-3T" in name_new) or ("4T-5T" in name_new):
+                    size_mapped = "toddler"
+
+                print(size_mapped+"\n")
+
+                # item_new = Item.object.filter(name == item_mapped + " " + size_mapped)
+                # item_new.quantity += (t.quantity if t.quantity>0 else 0)
+
+
+    def _add_items(self):
+        for t in Item.objects.all():
+            name = t.name
+            x = re.match("^(boys|girls)", name)
+            if x:
+                if name.startswith("boys "):
+                    name_new = name[len("boys "):]
+                elif name.startswith("girls "):
+                    name_new = name[len("girls "):]
+                elif 
+
+                item_mapped = name_new.split()[0]
+                if (len(name_new.split()) > 1) and name_new.split()[1] == "socks":
+                    item_mapped = "socks"
+                if ("snowsuit" in item_mapped):
+                    item_mapped = "snowsuit"
+                if !(item_mapped in NEW_ITEM_NAMES):
+
+        for item in NEW_ITEM_NAMES:
+            Item.objects.create(name=item_name, quantity=0)
+
+    def _add_outdated(self):
         for t in Item.objects.all():
             print(t.outdated)
             t.outdated = False
@@ -35,27 +77,52 @@ class Command(BaseCommand):
 
         print("Items update.")
 
-    def _update_item_transactions(self):
-        ItemTransaction.objects.all().delete() 
-
-        print("ItemTransactions deleted.")
-
     def _update_checkins(self):
-        
-        print(Checkin.objects.all())
-        print("\nCheckins updated.")
+        for ins in Checkin.objects.all():
+            print(ins)
 
-    def _update_checkouts(self):
-        Checkout.objects.all().delete() 
-
-        print("Checkouts deleted.")
 
     def handle(self, *args, **options):
-        # self._update_checkins()
+        self._add_outdated()
+        self._add_items()
+        self._update_items()
+        self._update_checkins()
         # self._update_checkouts()
         # self._update_item_transactions()
-        self._update_items()
         # self._update_categories()
         # self._update_age_ranges()
         # self._update_families()
         # self._update_users()
+
+
+# WANT: Baby, kid, toddler, teen
+
+# From Tomas Viejobueno to Everyone 11:33 AM
+# CURRENT: 0-24 mo -> baby
+# 2-14-> kid
+# 14-20 -> teen
+# 2-5 -> toddler
+# 6-14 -> kid
+# 14-20 -> teen
+# 2-5, infant -> toddler
+# 0-24 mo, infant -> baby
+# 2T-5T, toddler -> toddler
+# 6-14, kid -> kid
+# 14-20, teen -> teen
+
+# From Harriet Khang to Everyone 11:40 AM
+# Shirt 
+# Pant
+# Underwear
+# Socks 
+# Pjs
+# Shoe
+# Toy		
+# Blanket
+# Hygiene kit 
+# Stuffed Animal
+
+# From Tomas Viejobueno to Everyone 11:41 AM
+# Items: shirt, shoes, underwear, socks, pjs, shorts, pants, sweater, coat/jacket, dress/skirt
+
+# aggregate all checkouts items, delete others
